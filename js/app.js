aaa11e96f044c5719e306b96750a458a53f57ea6
RickDPA <100905827+RickDPA@users.noreply.github.com>
2022-11-09 20:02:03 -0600

    tercer commit
    ter

--- js/app.js
+++ js/app.js
@@ -28,19 +28,79 @@ function buildQuiz(){
 }
 
 
-function showResults(){}
+function showResults(){
+    const answerBoxs = quizBox.querySelectorAll('.answers');
+    let numCorrect = 0;
+    myQuestions.forEach( (currentQuestion, questionNumber) => {
+        const answerBox = answerBoxs[questionNumber];
+        const selector = `input[name=question${questionNumber}]:checked`;
+        const userAnswer = (answerBox.querySelector(selector) || {}).value;
+        if(userAnswer === currentQuestion.correctAnswer){
+            numCorrect++;
+            answerBoxs[questionNumber].style.color = 'green';
+        }
+        else{
+            answerBoxs[questionNumber].style.color = 'red';
+        }
+    });
+    resultsBox.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
+}
+
+function showSlide(n) {
+    slides[currentSlide].classList.remove('active-slide');
+    slides[n].classList.add('active-slide');
+    currentSlide = n;
+    if(currentSlide === 0){
+        previousButton.style.display = 'none';
+    }
+    else{
+        previousButton.style.display = 'inline-block';
+    }
+    
+    if(currentSlide === slides.length-1){
+        nextButton.style.display = 'none';
+        submitButton.style.display = 'inline-block';
+    }
+    else{
+        nextButton.style.display = 'inline-block';
+        submitButton.style.display = 'none';
+    }
+}
+
+function showNextSlide() {
+    showSlide(currentSlide + 1);
+}
+
+function showPreviousSlide() {
+    showSlide(currentSlide - 1);
+}
+
+
 
 
 const quizBox = document.getElementById('quiz');
 const resultsBox = document.getElementById('results');
 const submitButton = document.getElementById('submit');
+const tryAgainButton = document.getElementById("tryAgain");
+
+
 const myQuestions = [];
 
 
 for(i=0; i < data.length; i++){
-myQuestions.push(data[i]);
+    myQuestions.push(data[i]);
 }
+
+document.getElementById('quizLength').innerHTML = data.length;
 
 
 buildQuiz();
+const previousButton = document.getElementById("previous");
+const nextButton = document.getElementById("next");
+const slides = document.querySelectorAll(".slide");
+let currentSlide = 0;
+showSlide(currentSlide);
+
 submitButton.addEventListener('click', showResults);
+previousButton.addEventListener("click", showPreviousSlide);
+nextButton.addEventListener("click", showNextSlide);
